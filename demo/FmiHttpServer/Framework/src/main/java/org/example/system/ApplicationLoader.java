22988b3bd436ffa4cb42cde197f9182d500a2fb3
Martin Aleksandrov <martin_20.05@abv.bg>
2025-01-28 11:41:37 +0200

    Creating new features

--- demo/FmiHttpServer/Framework/src/main/java/org/example/system/ApplicationLoader.java
+++ demo/FmiHttpServer/Framework/src/main/java/org/example/system/ApplicationLoader.java
@@ -1,12 +1,14 @@
 package org.example.system;
 
-import org.example.steriotypes.Controller;
+import org.example.entities.ControllerMeta;
+import org.example.steriotypes.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import org.example.entities.RequestInfo;
 
@@ -16,20 +18,23 @@ public class ApplicationLoader {
 
     //Създаване на речник, в който да се пази комбинацията от HTTP method и HTTP endpoint
     // и класа който трябва да го обработи
-    private HashMap<RequestInfo, Class> controllerLookupTable = new HashMap<RequestInfo, Class>();
+    private HashMap<RequestInfo, ControllerMeta> controllerLookupTable = new HashMap<>();
 
     //Изпълнение на контролера
     public String executeController(String httpMethod, String httpEndPoint) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
 
-        Class clazz = controllerLookupTable.get(new RequestInfo(httpMethod ,httpEndPoint));
+        ControllerMeta controllerMethodReference = controllerLookupTable.get(new RequestInfo(httpMethod ,httpEndPoint));
 
-        if(clazz == null){
+        if (controllerMethodReference == null) {
             return "";
         }
+
+        Class clazz = controllerMethodReference.getClassReference();
+        String methodName = controllerMethodReference.getMethodName();
+
         //Тук имаме инстанция
         var controllerInstance =  clazz.getDeclaredConstructor().newInstance();
-
-        return  (String) clazz.getMethod("Index").invoke(controllerInstance);
+        return  (String) clazz.getMethod(methodName).invoke(controllerInstance);
     }
 
 
@@ -53,8 +58,8 @@ public class ApplicationLoader {
             }
 
             if(packageReference.contains(".class")){
-
                 //Тук се парсват класове
+                classParser(packageReference, packageName);
             }
         }
     }
@@ -67,18 +72,54 @@ public class ApplicationLoader {
 
         if(clazz.isAnnotationPresent(Controller.class)){
 
+            parseController(clazz);
 
         }
     }
 
     private  void parseController(Class clazz){
 
-        Controller annotation = (Controller) clazz.getAnnotation(Controller.class);
-        String httpMethod = annotation.method();
-        String httpEndPoint = annotation.endpoint();
+        //Взимане на всички методи на този контролер, но само ако са публични
 
-        controllerLookupTable.put(
-                new RequestInfo(httpMethod, httpEndPoint), clazz
-        );
+        Method[] controllerClassMethodCollection =  clazz.getMethods();
+
+        for (Method method : controllerClassMethodCollection) {
+
+            if(method.isAnnotationPresent(org.example.steriotypes.GetMapping.class)){
+
+                GetMapping annotation = method.getAnnotation(org.example.steriotypes.GetMapping.class);
+                String methodEndPoint = annotation.value();
+
+                controllerLookupTable.put(new RequestInfo("GET", methodEndPoint),
+                        new ControllerMeta(clazz, method.getName()));
+            }
+
+            if(method.isAnnotationPresent(org.example.steriotypes.PostMapping.class)){
+
+                PostMapping annotation = method.getAnnotation(org.example.steriotypes.PostMapping.class);
+                String methodEndPoint = annotation.value();
+
+                controllerLookupTable.put(new RequestInfo("POST", methodEndPoint),
+                        new ControllerMeta(clazz, method.getName()));
+            }
+
+            if(method.isAnnotationPresent(org.example.steriotypes.PutMapping.class)){
+
+                PutMapping annotation = method.getAnnotation(org.example.steriotypes.PutMapping.class);
+                String methodEndPoint = annotation.value();
+
+                controllerLookupTable.put(new RequestInfo("PUT", methodEndPoint),
+                        new ControllerMeta(clazz, method.getName()));
+            }
+
+            if(method.isAnnotationPresent(org.example.steriotypes.DeleteMapping.class)){
+
+                DeleteMapping annotation = method.getAnnotation(org.example.steriotypes.DeleteMapping.class);
+                String methodEndPoint = annotation.value();
+
+                controllerLookupTable.put(new RequestInfo("DELETE", methodEndPoint),
+                        new ControllerMeta(clazz, method.getName()));
+            }
+        }
     }
 }
